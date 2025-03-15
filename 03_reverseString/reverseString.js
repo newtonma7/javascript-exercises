1c7ffcec34f3fc1040f8436e123ae4939a5725e7
newtonma7 <newtonma7@gmail.com>
2025-03-15 14:45:48 -0400

    third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    ans = '';
+    for(i = str.length-1; i>=0; i--){
+        ans+=str[i];
+    }
+    return ans
 };
 
 // Do not edit below this line
