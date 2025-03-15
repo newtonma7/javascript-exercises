234080c929609b75eb3f421eb2d523794ee1d2f9
newtonma7 <newtonma7@gmail.com>
2025-03-15 15:36:39 -0400

    seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  if(f-32 === 0) return 0;
+  return Number(((f-32)*(5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  if(c===32) return 0;
+
+  return Number((((9/5)*(c)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
