44b6b2eb42b084dc6014b69548d75acab7887fa3
newtonma7 <newtonma7@gmail.com>
2025-03-15 15:11:11 -0400

    4,5 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    return arr.filter(x => !targets.includes(x));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    if(!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0){
+        return 'ERROR'
+    }
+    let sum = 0;
 
+    if (start > end) [start, end] = [end, start]
+    
+    for(let i = start; i <= end; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
